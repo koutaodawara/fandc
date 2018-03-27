9a84568088da3103e1c702afd0b8065341a0cbf3
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2018-03-27 17:24:58 +0900

    model rewrite

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,2 +1,10 @@
 class Home < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :company, presence: true
+  validates :telephone, presence: true
+  validates :postnum, presence: true
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :message, presence: true
 end
