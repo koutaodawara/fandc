448a362fc3483fcecade3aacb21c9a434e18af73
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2018-04-14 15:32:30 +0900

    delete validate

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,6 @@
 class Home < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
-  validates :company, presence: true
   validates :telephone, presence: true
   validates :postnum, presence: true
   validates :address, presence: true
